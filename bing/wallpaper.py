93bd96a02732fc34cb23fc1c81499f88cc3b30d2
jckling <jckling@163.com>
2021-04-21 09:27:58 +0800

    🔧 修改推送的 bing 壁纸大小

--- bing/wallpaper.py
+++ bing/wallpaper.py
@@ -7,7 +7,10 @@ from datetime import datetime
 
 import requests
 
-BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160"
+# 4k UHD
+# BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160"
+# 1080P HD
+BING_API = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=10&nc=1612409408851&pid=hp&FORM=BEHPTB"
 BING_URL = "https://cn.bing.com"
 
 
